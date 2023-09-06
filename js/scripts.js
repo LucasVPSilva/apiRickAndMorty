8b5cedeac80eba246ac5bc304e8a77280378dc70
Lucas Pires Silva <118729077+LucasVPSilva@users.noreply.github.com>
2023-09-05 23:40:01 -0300

    adicionado status para personagens

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,16 @@ const showListCharacter = document.querySelector('.characters-list');
 // const divImgCharacter = document.querySelector('.character-img');
 async function listCharacters() {
   try {
-    const response = await api.get("/character");
+    const response = await api.get("/character/?page=1");
     const characters = response.data.results;
 
 
     console.log(response);
     console.log(characters);
 
+
     characters.forEach(character => {
+
       // div principal
       const showCharacter = document.createElement("div");
       showCharacter.classList.add('character-card');
@@ -25,18 +27,37 @@ async function listCharacters() {
 
       sectionImg.innerHTML = `<img src="${character.image}"/>`
 
+
       // section dos textos:
 
       const sectionText = document.createElement("section");
       sectionText.classList.add('character-text');
       sectionText.innerHTML = `
-        <h3>${character.name}</h3>
-        <h5>${character.status} - ${character.species}</h5>
+        <h3 class='title-character'>${character.name}</h3>
+        <div class="cardStatus">
+          <div class='statusColor ${character.status == 'Dead'
+          ? 'dead' : character.status == 'Alive' ? 'alive'
+            : 'unknown'}'>
+         </div> 
+         <span>${character.status == 'Dead'
+          ? 'Morto' : character.status == 'Alive' ? 'Vivo'
+            : 'Desconhecido'}</span> &nbsp - &nbsp <span> ${character.species} </span>
+        </div>
         <p>Última localização conhecida:</p>
         <h4>${character.location.name}</h4>
-        <p>Ultima aparição nos episodios:</p>
-        <h4>${character.episode.length}</h4>
+
       `
+
+
+
+
+      // sectionText.innerHTML = `
+      //   <h3>${character.name}</h3>
+      //   <h5>${character.status} - ${character.species}</h5>
+      //   <p>Última localização conhecida:</p>
+      //   <h4>${character.location.name}</h4>
+
+      // `
 
 
       // fazendo os appendChild
